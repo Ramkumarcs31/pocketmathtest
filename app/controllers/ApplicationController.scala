17f4e5e9512b0b030b202e17f61ad0da429177eb
Ramkumar V <ramkumar@bidstalk.com>
2016-11-30 10:25:56 +0530

    code change

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -4,61 +4,61 @@ import javax.inject.Inject
 
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, Controller}
-import services.PocketMathService
+import main.PocketMath
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class ApplicationController @Inject()(pocketMathService: PocketMathService) extends Controller {
+class ApplicationController @Inject()(pocketMath: PocketMath) extends Controller {
 
-  def traders(maybeCity: Option[String]): Action[AnyContent] = Action.async {
-    pocketMathService.getTraders(maybeCity) map {
+  def traders(City: Option[String]): Action[AnyContent] = Action.async {
+    pocketMath.getTraders(City) map {
       case Some(traders) =>
-        Ok(Json.obj("status" -> Json.obj("code" -> 1000, "msg" -> "Traders data found"), "data" -> traders))
+        Ok(Json.obj("status" -> Json.obj("code" -> 100, "msg" -> "Got Data"), "data" -> traders))
       case None =>
-        NotFound(Json.obj("status" -> Json.obj("code" -> 1005, "msg" -> "No Traders data found")))
+        NotFound(Json.obj("status" -> Json.obj("code" -> 101, "msg" -> "Data Not Found")))
     } recover {
       case t: Throwable =>
         t.printStackTrace
-        InternalServerError(Json.obj("status" -> Json.obj("code" -> 1001, "msg" -> "Failed to retrieve traders")))
+        InternalServerError(Json.obj("status" -> Json.obj("code" -> 404, "msg" -> "Failed to retrieve Data")))
     }
   }
 
-  def transactions(maybeYear: Option[Int]): Action[AnyContent] = Action.async {
-    pocketMathService.getTransactions(maybeYear) map {
+  def transactions(Year: Option[Int]): Action[AnyContent] = Action.async {
+    pocketMath.getTransactions(Year) map {
       case Some(transactions) =>
-        Ok(Json.obj("status" -> Json.obj("code" -> 2000, "msg" -> "Transactions data found"), "data" -> transactions))
+        Ok(Json.obj("status" -> Json.obj("code" -> 100, "msg" -> "Got Data"), "data" -> transactions))
       case None =>
-        NotFound(Json.obj("status" -> Json.obj("code" -> 2005, "msg" -> "No transactions data found")))
+        NotFound(Json.obj("status" -> Json.obj("code" -> 101, "msg" -> "Data Not Found")))
     } recover {
       case t: Throwable =>
         t.printStackTrace
-        InternalServerError(Json.obj("status" -> Json.obj("code" -> 2001, "msg" -> "Failed to retrieve transactions")))
+        InternalServerError(Json.obj("status" -> Json.obj("code" -> 404, "msg" -> "Failed to retrieve Data")))
     }
   }
 
-  def avgTransactions(maybeCity: Option[String]): Action[AnyContent] = Action.async {
-    pocketMathService.getTransactionsAvg(maybeCity) map {
+  def avgTransactions(City: Option[String]): Action[AnyContent] = Action.async {
+    pocketMath.getTransactionsAvg(City) map {
       case Some(transactions) =>
-        Ok(Json.obj("status" -> Json.obj("code" -> 3000, "msg" -> "Transactions data found"), "data" -> transactions))
+        Ok(Json.obj("status" -> Json.obj("code" -> 100, "msg" -> "Got Data"), "data" -> transactions))
       case None =>
-        NotFound(Json.obj("status" -> Json.obj("code" -> 3005, "msg" -> "No transactions data found")))
+        NotFound(Json.obj("status" -> Json.obj("code" -> 101, "msg" -> "Data Not Found")))
     } recover {
       case t: Throwable =>
         t.printStackTrace
-        InternalServerError(Json.obj("status" -> Json.obj("code" -> 3001, "msg" -> "Failed to retrieve transactions")))
+        InternalServerError(Json.obj("status" -> Json.obj("code" -> 404, "msg" -> "Failed to retrieve Data")))
     }
   }
 
   def transaction(value: String): Action[AnyContent] = Action.async {
-    pocketMathService.getTransaction(value) map {
+    pocketMath.getTransaction(value) map {
       case Some(transaction) =>
-        Ok(Json.obj("status" -> Json.obj("code" -> 4000, "msg" -> "Transaction data found"), "data" -> transaction))
+        Ok(Json.obj("status" -> Json.obj("code" -> 100, "msg" -> "Got Data"), "data" -> transaction))
       case None =>
-        NotFound(Json.obj("status" -> Json.obj("code" -> 4005, "msg" -> "No transaction data found")))
+        NotFound(Json.obj("status" -> Json.obj("code" -> 101, "msg" -> "Data Not Found")))
     } recover {
       case t: Throwable =>
         t.printStackTrace
-        InternalServerError(Json.obj("status" -> Json.obj("code" -> 4001, "msg" -> "Failed to retrieve transaction")))
+        InternalServerError(Json.obj("status" -> Json.obj("code" -> 404, "msg" -> "Failed to retrieve Data")))
     }
   }
 
